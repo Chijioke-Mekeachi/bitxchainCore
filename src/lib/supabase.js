2b75040376662010ec22a011a2b362106d8c5bf9
Chijioke-Mekeachi <chijiokemekelachi@gmail.com>
2025-08-03 09:57:56 +0100

    _

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://xbifbtbouraistmakxwj.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhiaWZidGJvdXJhaXN0bWFreHdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NTU3MTEsImV4cCI6MjA2NTMzMTcxMX0.fxb-VlYG_sBdGahWFk-q8mAKD6yOyMqSZTkdkoTCXLg";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
